9c0853a873859fcb55c3a16f91d1e152813e11e6
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-29 14:55:20 +0300

    add variables and set variables via useState

--- Calculator-ReactJS/src/Calculator.jsx
+++ Calculator-ReactJS/src/Calculator.jsx
@@ -1,28 +1,40 @@
+import { useState } from 'react'
 import './Calculator.css'
 
 
 export default function Calculator(){
+    const[display, setDisplay]=useState('')
+    const [currentValue, setCurrentValue]=useState('')
+    const[previousValue, setPreviousValue]=useState('')
+    const[operator, setOperator]=useState(null)
+    const [justCalculated, setJustCalculated]=useState(false)
+
+    function handleNumber(number){
+        
+    }
+
+    
 
     return(
         <div className="calculator">
-  <div className="display"></div>
+  <div className="display">{display || "0"}</div>
   <div className='buttons'>
     <button className='clear'>C</button>
-    <button>9</button>
-    <button>8</button>
+    <button onClick={()=>handleNumber('9')}>9</button>
+    <button onClick={()=>handleNumber('8')}>8</button>
     <button className='operator'>/</button>
-    <button>7</button>
+    <button onClick={()=>handleNumber('7')}>7</button>
 
-    <button>6</button>
-    <button>5</button>
+    <button onClick={()=>handleNumber('6')}>6</button>
+    <button onClick={()=>handleNumber('5')}>5</button>
     <button className='operator'>*</button>
-    <button>4</button>
+    <button onClick={()=>handleNumber('4')}>4</button>
 
-    <button>3</button>
-    <button>2</button>
+    <button onClick={()=>handleNumber('3')}>3</button>
+    <button onClick={()=>handleNumber('2')}>2</button>
     <button className='operator'>-</button>
-    <button>1</button>
-    <button>0</button>
+    <button onClick={()=>handleNumber('1')}>1</button>
+    <button onClick={()=>handleNumber('0')}>0</button>
 
     <button>.</button>
     <button className='operator'>+</button>
