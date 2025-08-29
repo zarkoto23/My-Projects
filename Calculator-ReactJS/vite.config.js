7629388db1e4c1f70cbf369c8b888522fbc218be
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-29 17:30:22 +0300

    Update vite.config.js

--- Calculator-ReactJS/vite.config.js
+++ Calculator-ReactJS/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/My-Projects/Calculator-ReactJS/",
+  base: "/Calculator-ReactJS/",
   plugins: [react()],
 });
