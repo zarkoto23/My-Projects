16826eec66bac99ab45d99c26fd1d12a47189a93
Svetozar Stoev <zarkoto23@gmail.com>
2025-10-30 18:27:12 +0200

    s

--- Game-Play-Study-Project/src/App.jsx
+++ Game-Play-Study-Project/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import { Routes, Route } from "react-router";
 import Login from "./components/login/Login";
